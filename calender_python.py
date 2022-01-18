b6c657271efaf7b0e091f7a1492bb167d9bcaf83
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-01-18 22:19:18 +0700

    halo

---
+++ calender_python.py
@@ -0,0 +1,11 @@
+import calendar
+
+yy = int(input("tahun berapa yang andainginkan :"))
+mm = int(input("bulan berapa yang anda inginkan :"))
+
+print(calendar.month(yy,mm))
+
+#dalam kasus ini kita menggunakan fungsi import
+#yang diimport adalah modul pythonnya
+#modul pyhton adalah sebuah file untuk memecah program yg berukuran besar ke program yg lebih kecil.
+#import bersungsi sebagai multi file,kita dapat memanggil file lain didalam satu modul yang berbeda
